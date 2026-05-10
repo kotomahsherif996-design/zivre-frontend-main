2d6c0278ff113be4b586b45611b4830827bddd2a
kotomahsherif996-design <kotomahsherif996@gmail.com>
2026-05-10 20:26:28 +0000

    Update CustomerDashboard.jsx

--- src/dashboard/CustomerDashboard.jsx
+++ src/dashboard/CustomerDashboard.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import BottomNav from '../common/BottomNav'
 import { getPaymentSettings } from '../api/client'
 import { useAuth } from '../contexts/AuthContext'
 import RoleBasedTour from '../common/RoleBasedTour'
@@ -25,7 +26,6 @@ import {
   Home as HomeIcon, Build as BuildIcon, Person as PersonIcon
 } from '@mui/icons-material'
 import PaymentFlier from '../common/PaymentFlier'
-import BottomNav from '../common/BottomNav'
 import MessageIcon from '@mui/icons-material/Message'
 import CustomServiceModal from '../common/CustomServiceModal'
 import Header from '../layout/Header'
